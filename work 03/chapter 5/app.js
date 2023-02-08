acaf428720c43e683339a65d703a830ea609ad1d
mhashimabbasi <mhashimabbasi2311@gmail.com>
2023-02-09 02:17:27 +0500

    class 03 JS
    Assignment 03
    chapter 05 to 09

---
+++ work 03/chapter 5/app.js
@@ -0,0 +1,302 @@
+        // Assignment 03
+    // Chapter no 05
+
+    document.write('<h1> "Chapter no 05 " </h1>')
+
+    document.write('<h2> "Assignment 03 " </h2>')
+
+    // Question no 01
+
+    document.write('<h3> "Example Ques. # 01 " </h3>')
+    
+    document.write("<h3>Addition:</h3>")
+    var a= 3
+    var b= 5
+    var c= a + b
+    document.write("Sum of" + " " + a + " " + "and" +" " + b + " "+ "is" + " " + c )
+
+
+    // Question No 02 (A)
+
+    document.write('<h3> "Example Ques. # 02 A " </h3>')
+
+    document.write("<h3>Multipication:</h3>")
+    var ma=3
+    var mb=5
+    var mc= ma * mb
+    document.write("Multiply of" + " " + ma + " " +  "and" + " " + mb + " " + "is" + " " + mc )
+
+    // Question No 02 (B)
+
+    document.write('<h3> "Example Ques. # 02 B" </h3>')
+
+    document.write("<h3>Division:</h3>")
+    var da=5
+    var db=3
+    var dc= ma / mb
+    document.write("Divide of" + " " + da + " " +  "and" + " " + db + " " + "is" + " " + dc )
+ 
+     // Question No 02 (C)
+
+     document.write('<h3> "Example Ques. # 02 C " </h3>')
+
+    document.write("<h3>Modulus:</h3>")
+    var moa=5
+    var mob=3
+    var moc= moa % mob
+    document.write("Divide of" + " " + moa + " " +  "and" + " " + mob + " " + "is" + " " + moc + "<br>")
+ 
+
+    // Question No 03 
+
+    document.write('<h3> "Example Ques. # 03 A " </h3>')
+   
+    var a =
+    document.write( "Value after variable declaration is" + " " + num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 B " </h3>')
+    var num= 5
+    document.write( "Intial value:" + num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 C " </h3>')
+    var num= 5
+    document.write("Value after increment is:" + " " + ++ num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 D " </h3>')
+    var num= 4
+    document.write("Value after increment is:" + " " + ++ num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 E(a) " </h3>')
+    var numa=5
+    var numb=8
+    var numc = numa + 8
+    document.write( "Value after 2 Variable Addtion is:" + " " + numc)
+
+
+
+    document.write('<h3> "Example Ques. # 03 E(b) " </h3>')
+    var num=4
+    var numa=5
+    var numc = num + numa
+    document.write("Value after increment is:" + " " + ++ numc)
+
+
+
+    document.write('<h3> "Example Ques. # 03 F " </h3>')
+    var num=5
+    var numa = ++num
+    document.write("Value after increment is:" + " " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 G " </h3>')
+    var num=6
+    var numa = num
+    document.write("Value 6+1 after increment is:" + " " + ++num)
+
+
+
+    document.write('<h3> "Example Ques. # 03 H " </h3>')
+    var a= 6
+    var b= 6
+    var c= a+b 
+    document.write("Value after increment is 6+6 =" + ++c )
+
+
+
+    document.write('<h3> "Example Ques. # 03 I " </h3>')
+    var num= 6
+    var numa = --num
+    document.write("Decrement the variable. 6-1 = " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 J" </h3>')
+    var a= 19
+    var b= 6
+    var c= a-b 
+    document.write("Value after Decrement is 19-6 =" + --c )
+
+
+
+    document.write('<h3> "Example Ques. # 03 K" </h3>')
+    var num=10
+    var numa = num / 3
+    document.write("After 10 dividing the variable's value by 3:" + " " + "=" + " " + numa)
+
+
+
+    document.write('<h3> "Example Ques. # 03 L" </h3>')
+    var a=12
+    var b=3
+    var c= a%b
+
+    document.write("After 12 Modulus the variable's value by 3:"  + " " + "=" + " " + c)
+
+    // Question No 04
+
+    document.write('<h3> "Ques. # 04 " </h3>')
+
+    document.write("<h5>Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.?</h5>")
+  
+
+    document.write('<h3> "Example Ques. # 04 " </h3>')
+    var ticket =600
+    var buying =5
+    var total = ticket * buying
+    document.write("Total cost to buy 5 tickets to a movie is " + " " + total + " " + "PKR.")
+
+
+    // Question No 05
+
+    document.write('<h3> "Explain. # 05 " </h3>')
+    document.write('<h3> "Table of 3" </h3>')
+
+    var aa = 1
+    var ab = 2
+    var ac = 3
+    var ad = 4
+    var ae = 5
+    var af = 6
+    var ag = 7
+    var ah = 8
+    var ai = 9
+    var aj = 10
+
+    var ta = ac * aa  
+    document.write("3x1=" + ta + "<br>")
+
+    var tb = ac * ab
+    document.write("3x2=" + tb + "<br>")
+
+    var tc = ac * ac 
+    document.write("3x3=" + tc + "<br>")
+
+    var td = ac * ad
+    document.write("3x4=" + td + "<br>")
+
+    var te =  ac * ae
+    document.write("3x5=" + te + "<br>")
+
+    var tf = ac * af
+    document.write("3x6=" + tf + "<br>")
+
+    var tg = ac * ag
+    document.write("3x7=" + tg + "<br>")
+
+    var th = ac * ah
+    document.write("3x8=" + th + "<br>")
+    
+    var ti = ac * ai
+    document.write("3x9=" + ti + "<br>")
+
+    var tj = ac * aj
+    document.write("3x10=" + tj + "<br>")
+
+
+    // Question No 06
+
+    document.write('<h3> "Explain. # 06 " </h3>')
+
+        var tem_c = 16;
+        var f = (tem_c * 9/5)+32
+        document.write(tem_c + "<sup>0</sup> C is" + f + "<sup>0</sup> F <br>");
+        
+        var tem_f = 60;
+        var c = (tem_f - 32) * 5/9;
+        document.write(tem_f + "<sup>0</sup> F is" + c +  "<sup>0</sup> C");
+
+
+
+    // Question No 07
+
+    document.write('<h3> "Explain. # 07 " </h3>')
+
+    document.write("<h2>Shopping Cart</h2>")
+    var total = 650 * 3 + 100 * 7 + 100
+    document.write("Price of item 1 is 650 <br>")
+    document.write("Quantity of item 1 is 3 <br>")
+    document.write("Price of item 2 is 100 <br>")
+    document.write("Quantity of item 2 is 7 <br>")
+    document.write("Shipping Charges 100 <br><br><br>")
+    document.write("Total cost of your order is" + " " + total)
+
+   // Question No 08
+
+   document.write('<h3> "Explain. # 08 " </h3>')
+
+    document.write("<h2>Marks Sheet</h2>")
+    var total_marks = 980
+    var marks_obtained = 804
+    document.write("Marks Obtaied: 804<br>")
+    document.write("Total Marks: 980 <br>")
+    document.write("Percentage:" + marks_obtained * 100 / total_marks + "%") 
+
+    // Question No 09
+
+    document.write('<h3> "Explain. # 09 " </h3>')
+
+    document.write("<h2>Currency in PKR</h2>")
+
+    var total = 10 * 2751.54 + 25 * 1833.50
+    document.write("Total Currency in PKR = " + total + "Rupee") 
+
+
+    // Question No 10
+
+    document.write('<h3> "Explain. # 10 " </h3>')
+
+
+    var num = 5
+    
+    document.write("Addition =" + " " + (num+5) +"<br>" + "Multiplication=" + " " + (num*10) + "<br>" +"Division=" + " " + (num/2) )
+
+
+     // Question No 11
+
+    document.write('<h3> "Explain. # 11 " </h3>')
+
+    document.write("<h2>Age Calculator</h2>")
+
+    var current_year = 2023
+    var bith_year = 1999
+    document.write("Current Year" + current_year + "<br>")
+    document.write("Birth Year" + bith_year + "<br>")
+    document.write("Your Age is:"+ (current_year - bith_year))
+
+    
+     // Question No 12
+
+    document.write('<h3> "Explain. # 12 " </h3>')
+
+     document.write("<h2>The Geometrizer</h2>")
+     var radius = 20
+    document.write("Radius of a Circle: 20" + "<br>")
+    document.write("The Circumeference is: " + 3.142 * 2 + "<br>")
+    document.write("The Area is: " + 6.284 * 20)
+
+    
+     // Question No 13
+
+    document.write('<h3> "Explain. # 13 " </h3>')
+
+    document.write('<h2>"The Lifetime Supply Calculator"</h2>')
+    var fav= "French Fries"
+    var age= 24
+    var max_age= 65
+    var ammount= 10
+    var total_amount = (max_age - age) * 365 * ammount
+    document.write("Favourite Snacks:" + " " + fav +  "<br>")
+    document.write("Current age: 24 <br>")
+    document.write("Estimated Maximum Age: 65 <br>")
+    document.write("Amount of snacks per day: 50 <br>")
+    document.write("I Will need" + " " + total_amount + "/= " + " " + fav + " " + " to last your until the ripe Old age of 65.")
+
---
+++ work 03/chapter 6-9/app.js
@@ -0,0 +1,116 @@
+//  // Chapter # 6-9
+//  // Question No 01
+
+// document.write('<h2>"Explain Quest # 01"</h2>')
+
+// document.write("<h3>Result:</h3>")
+// var a=10
+// document.write("The value is of a is:" + " " + a + "<br><br>")
+// document.write("The value of ++a is:" + " " + ++a + "<br>")
+
+// document.write("Now value of of a is:" + " " +a +"<br><br>")
+
+// document.write("The value of a++ is:" + " " + a++ +"<br>")
+// document.write("Now value of of a is:" + " " + a +"<br><br>")
+
+// document.write("The value of --a is:" + " " + --a + "<br>")
+// document.write("Now value of of a is:" + " " + a +"<br><br>")
+
+// document.write("The value of a-- is:" + " " + a-- + "<br>") 
+// document.write("Now value of of a is:" + " " + a +"<br><br>")
+
+// // Question No 02
+
+// document.write('<h2>"Explain Quest # 02"</h2>')
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b +b--
+// document.write("a is = 2 <br>")
+// document.write("b is = 1<br>")
+// document.write("result is= " + result)
+
+
+// // Question No 03
+
+// document.write('<h2>"Explain Quest # 03"</h2>')
+
+// var fullName =prompt("Enter your name")
+// document.write("Hello" + " " + fullName + " " +  "Nice to meet you!<br>")
+
+// var phoneNumber =prompt("Enter your Contact number","+92 3")
+// document.write("Thank's to give me a contact number" + " " + phoneNumber + " " + "nice Easy number")
+
+
+// // Question No 04
+
+// document.write('<h2>"Explain Quest # 04"</h2>')
+
+
+// var userNum = prompt("Enter your number that yoy find Table","5")
+// document.write("<h2>"+"Table of  "+ userNum + "</h2>")
+// document.write(userNum + " x 1 =" + " " + userNum * 1 +"<br>")
+// document.write(userNum + " x 2 =" + " " + userNum * 2 +"<br>")
+// document.write(userNum + " x 3 =" + " " + userNum * 3 +"<br>")
+// document.write(userNum + " x 4 =" + " " + userNum * 4 +"<br>")
+// document.write(userNum + " x 5 =" + " " + userNum * 5 +"<br>")
+// document.write(userNum + " x 6 =" + " " + userNum * 6 +"<br>")
+// document.write(userNum + " x 7 =" + " " + userNum * 7 +"<br>")
+// document.write(userNum + " x 8 =" + " " + userNum * 8 +"<br>")
+// document.write(userNum + " x 9 =" + " " + userNum * 9 +"<br>")
+// document.write(userNum + " x 10 =" + " " + userNum * 10 +"<br>")
+
+
+// // Question No 06
+
+// document.write('<h2>"Explain Quest # 05"</h2>')
+
+// var urdu = prompt("Enter the subject name 1")
+// var math = prompt("Enter the subject name 2")
+// var eng = prompt("Enter the subject name 3")
+
+// var totalMarks = 100
+
+// var obtainedMarks_1 = +prompt("Enter subject 1 obtained marks") 
+// var obtainedMarks_2 = +prompt("Enter subject 2 obtained marks") 
+// var obtainedMarks_3 = +prompt("Enter subject 3 obtained marks") 
+
+// var subject_1_Percentage = obtainedMarks_1 / totalMarks * 100
+// var subject_2_Percentage = obtainedMarks_2 / totalMarks * 100
+// var subject_3_Percentage = obtainedMarks_3 / totalMarks * 100
+
+// var allSubTotalMarks =300
+// var allSub_Obt_MarksTotal = obtainedMarks_1 + obtainedMarks_2 + obtainedMarks_3;
+// var allSub_Total_Percentage = allSub_Obt_MarksTotal / allSubTotalMarks * 100
+
+// document.write ("<table>" 
+//                         + "<tr>" 
+//                             + "<th>" + "Subject" + "</th>" 
+//                             + "<th>" + "Total Marks" + "</th>" 
+//                             + "<th>" + "Obtained Marks" + "</th>" 
+//                             + "<th>" + "Percentage" + "</th>" 
+//                         + "</tr>" 
+//                         + "<tr>" 
+//                             + "<td>" + urdu + "</td>" 
+//                             + "<td>" + totalMarks + "</td>" 
+//                             + "<td>" + obtainedMarks_1 + "</td>" 
+//                             + "<td>" + subject_1_Percentage + "%" + "</td>" 
+//                         + "</tr>"
+//                         + "<tr>" 
+//                             + "<td>" + math + "</td>" 
+//                             + "<td>" + totalMarks + "</td>" 
+//                             + "<td>" + obtainedMarks_2 + "</td>" 
+//                             + "<td>" + subject_2_Percentage + "%" + "</td>" 
+//                         + "</tr>"
+//                         + "<tr>" 
+//                             + "<td>" + eng + "</td>" 
+//                             + "<td>" + totalMarks + "</td>" 
+//                             + "<td>" + obtainedMarks_3 + "</td>" 
+//                             + "<td>" + subject_3_Percentage + "%" + "</td>" 
+//                         + "</tr>"
+//                         + "<tr>" 
+//                             + "<th>" + "Grand Total" + "</th>" 
+//                             + "<td>" + allSubTotalMarks + "</td>" 
+//                             + "<td>" + allSub_Obt_MarksTotal + "</td>" 
+//                             + "<td>" + allSub_Total_Percentage + "%" + "</td>" 
+//                         + "</tr>"
+//                 + "</table>")
