572507a22d92388d0f7173d255a48bd3aa9da41b
mhashimabbasi <mhashimabbasi2311@gmail.com>
2023-02-09 02:14:26 +0500

    add new file
    assignment # 02

---
+++ Work 2/01/app.js
@@ -0,0 +1,33 @@
+//**********Chap no 2**********
+// Question no 1
+var username 
+alert("M. Hashim")
+
+
+// Question no 2
+alert("25 years old")
+
+
+// Question no 3
+alert("Inprocess Web Development & Mobile Hybrid  Application")
+
+
+// Question no 4
+// alert("15 years old")
+
+
+// Question no 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP\n")
+
+
+// Question no 6
+alert("My email address is mhashimabbasi2311@gmail.com")
+
+
+// Question no 7
+alert("I am trying to learn from the Book A smarter way to learn Javascript")
+
+
+
+// Question no 8
+document.write = "(<h1> Yah! I can write HTML content through JavaScript </h1>)"
---
+++ Work 2/01/new.js
@@ -0,0 +1,27 @@
+//       Assignment # 2 JAVASCRIPT
+//       Chap no 2
+
+    // Question no 1
+var username
+
+
+    // Question no 2
+var username= "M Hashim Abbasi"
+
+
+    //Question no 3
+
+// A) 
+var message
+
+// b) 
+var message= "Hellow World."
+
+// C)
+alert(message)
+
+
+    //Question no 04
+
+var data=" Name: M Hashim Abbasi. \n Age: 25Yrs \n Insitution: Student in SMIT \n Course: WMA B9. \n Email Adress: mhashimabbasi2311@gmail.com \n Book Name: I am trying to learn from the Book A smarter way to learn Javascript \n “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+alert(data)
